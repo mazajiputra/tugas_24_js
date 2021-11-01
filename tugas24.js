2a508d66fc8c8d6d47a554be00f6e2bdd238f661
mazajiputra <mazajiputra@gmail.com>
2021-11-01 21:13:03 +0700

    Kumpul Tugas

---
+++ tugas24.js
@@ -0,0 +1,11 @@
+var array1 = [32,33,16,40,9,7,11,2,44,1,66];
+function sorting(data) {
+    return data.sort()
+}
+
+function reverse(data) {
+    return data.reverse()
+}
+console.log("Sebelumnya :",array1);
+console.log("Ascending :",sorting(array1));
+console.log("Descending :",reverse(array1));
